8eacedb219a8196a69e50b49db6cb71ed9dc67be
burduliluka7 <63206890+burduliluka7@users.noreply.github.com>
2021-03-08 14:17:36 +0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace serialize_Experiment
+{
+    class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public double Age { get; private set; }
+        public Class1(int id, string name, string lastName, double age)
+            {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Age = age;
+
+           
+        }
+        public string GetInfo()
+        {
+            return $"user id is: {Id}, Name: {Name}, LastName: {LastName}, Age:{Age};";
+
+        }
+        public void SerializeNow()
+        {
+            File f = new File("temp.dat");
+            Stream s = f.Open(FileMode.Create);
+            BinaryFormatter b = new BinaryFormatter();
+            b.Serialize(s, Id);
+            s.Close();
+        }
+         
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace serialize_Experiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 klas = new Class1(434, "Luka", "Burduli", 16);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(klas.GetInfo());
+
+
+
+        }
+    }
+}
